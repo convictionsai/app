73ee76c4ba28bbee0f32ce9ebdd459cf11e5ea0e
Matthew Davis <matthew@matthewdavis.io>
2023-04-04 19:56:44 -0400

    bump

--- deploy.tf
+++ deploy.tf
@@ -1,203 +1,60 @@
 terraform {
-  required_providers {
-    civo = {
-      source  = "civo/civo"
-      version = "1.0.31" # Use the last available version
+    backend "s3" {
+        endpoint                    = "https://objectstore.nyc1.civo.com"
+        bucket                      = "states"
+        key                         = "app.tfstate"
+        region                      = "NYC1"
+        skip_region_validation      = true
+        skip_credentials_validation = true
+        skip_metadata_api_check     = true
+        force_path_style            = true
     }
-  }
 }
 
-provider "civo" {
-  token = var.civo_api_token
-}
+variable "name" {}
+variable "tag" {}
+variable "host" {}
+variable "token" {}
 
-data "civo_kubernetes_cluster" "my-cluster" {
-  name = "sandbox"
+provider "kubernetes" {
+    host     = var.host
+    token    = var.token
+    insecure = true
 }
 
 locals {
-  kubeconfig_map = yamldecode(data.civo_kubernetes_cluster.my-cluster.kubeconfig)
-}
-
-provider "kubernetes" {
-  host                   = data.civo_kubernetes_cluster.my-cluster.api_endpoint
-  client_key             = base64decode(local.kubeconfig_map.users[0].user.client-key-data)
-  cluster_ca_certificate = base64decode(local.kubeconfig_map.clusters[0].cluster.certificate-authority-data)
-  client_certificate     = base64decode(local.kubeconfig_map.users[0].user.client-certificate-data)
-}
-
-terraform {
-  backend "s3" {
-    endpoint                    = "https://objectstore.nyc1.civo.com"
-    bucket                      = "states"
-    key                         = "app.tfstate"
-    region                      = "NYC1"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
-}
-
-module "deployment-frontend-app" {
-
-  source = "github.com/convictionsai/terraform-kubernetes-deployment"
-
-  settings = {
-    name      = "app"
-    type      = "frontend"
-    version   = "0.0.6"
-    namespace = "convictionsai"
-    environment = "dev"
-    resources = {
-      replicas = 1
-      cpu      = "500m"
-      memory   = "512Mi"
-    }
-    networking = {
-      ports = [
-        {
-          name          = "http"
-          containerPort = 8080
-          targetPort    = 8080
-          protocol      = "TCP"
-
+    settings = {
+        name      = var.name
+        namespace = "convictionsai"
+        type      = "frontend"
+        version   = var.tag
+        resources = {
+            replicas = 1
+            requests = {
+                cpu    = "500m"
+                memory = "500Mi"
+            }
+            limits = {
+                cpu    = "500m"
+                memory = "500Mi"
+            }
         }
-      ]
-      ingress = {
-        hostname = "app.convictions.ai"
-        path     = "/"
-      }
-    }
-            env = {
-            PORT        = 8080
-            DB_HOST     = "mysql"
-            DB_PORT     = 3306
-            DB_USERNAME = "changeme"
-            DB_PASSWORD = "changeme"
+        networking = {
+            ingress = {
+                hostname = "app.convictions.ai"
+                path     = "/"
+            }
+            ports = [
+                {
+                    name = "http"
+                    port = 8080
+                }
+            ]
         }
-  }
+    }
 }
 
-resource "kubernetes_service_account" "cicd" {
-  metadata {
-    name      = "cicd"
-    namespace = "convictionsai"
-  }
-
-  lifecycle {
-    ignore_changes = [
-        secret
-    ]
-  }
-}
-
-resource "kubernetes_cluster_role" "cicd" {
-  metadata {
-    name = "cicd"
-  }
-
-  rule {
-    api_groups = [
-      ""
-    ]
-    resources = [
-      "componentstatuses"
-    ]
-    verbs = [
-      "list"
-    ]
-  }
-
-  rule {
-    api_groups = [
-      ""
-    ]
-    resources = [
-      "componentstatuses"
-    ]
-    verbs = [
-      "list"
-    ]
-  }
-
-  rule {
-    api_groups = [
-      "networking.k8s.io"
-    ]
-    resources = [
-      "ingresses"
-    ]
-    verbs = [
-      "get",
-      "create",
-      "update",
-      "delete"
-    ]
-  }
-
-  rule {
-    api_groups = [
-      ""
-    ]
-    resources = [
-      "services"
-    ]
-    verbs = [
-      "get",
-      "create",
-      "update",
-      "delete"
-    ]
-  }
-
-  rule {
-    api_groups = [
-      "apps"
-    ]
-    resources = [
-      "deployments"
-    ]
-    verbs = [
-      "get",
-      "create",
-      "update",
-      "delete"
-    ]
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "cicd" {
-  metadata {
-    name = "cicd"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cicd"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "cicd"
-    namespace = "convictionsai"
-
-  }
-}
-
-/* variable "KUBERNETES_SERVER" {
-  type        = string
-  description = "Kubernetes server endpoint for the TF Kubernetes provider"
-}
-
-variable "KUBERNETES_TOKEN" {
-  type        = string
-  description = "Kubernetes token to auth with the endpoint for the TF Kubernetes provider"
-} */
-
-variable "civo_api_token" {
-  description = "Civo API token"
-  type        = string
-  sensitive   = true
+module "deploy" {
+    source   = "github.com/convictionsai/terraform-kubernetes-deployment.git?ref=0.0.2"
+    settings = local.settings
 }
